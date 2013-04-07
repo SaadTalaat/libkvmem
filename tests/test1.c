9782a99d77403fa1e1b57eb330e270ac6639501e
Saad Talaat <saad.talaat@synapse-labs.com>
2013-04-07 12:12:59 +0200

    Adding a mainstream test.. and it works.

---
+++ tests/test1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <kvmem.h>
+#include <stdlib.h>
+#include <a.out.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int 
+main(void)
+{
+	struct nlist l[] = {{NULL},{NULL},{NULL},};
+	kvmem_t *kd;
+	l[1].n_un.n_name = "sys_call_table";
+	kd = kvmem_openfiles (NULL, NULL, O_RDWR, 1);
+	kvmem_nlist(kd, l);
+	printf("%lx\n", l[1].n_value);
+
+
+}
